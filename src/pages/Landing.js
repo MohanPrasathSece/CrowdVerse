e5e4d25c61317c48bb6065194d111d5abbff6e8b
MohanPrasathSece <mohanprasath.s2023ece@sece.ac.in>
2025-12-01 15:06:48 +0530

    new

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -27,7 +27,7 @@ const Landing = () => {
                 to="/signup"
                 className="w-full sm:w-auto px-6 py-2.5 sm:px-8 sm:py-3 sm:py-4 bg-off-white text-primary-black font-medium rounded-lg hover:bg-white transition-colors hero-cta-primary text-sm sm:text-base"
               >
-                Start Trading
+                Sign Up
               </Link>
               <Link
                 to="/login"
